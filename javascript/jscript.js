cd916d2019bacc07de514b548c394f4307d5f2c1
KavyaSJ <ksjadramakunti@gmail.com>
2025-05-15 15:56:18 -0400

    commit

---
+++ javascript/jscript.js
@@ -0,0 +1,31 @@
+// Get the button element by its ID
+const button1 = document.getElementById('nav-btn1');
+
+// Add a click event listener to the button
+button1.addEventListener('click', function() {
+  // Change the window location to a new URL
+  window.location.href ="bags1.html";  // Replace with your desired URL
+});
+
+const button2 = document.getElementById('nav-btn2');
+
+// Add a click event listener to the button
+button2.addEventListener('click', function() {
+  // Change the window location to a new URL
+  window.location.href ="bags2.html";  // Replace with your desired URL
+});
+
+const button3 = document.getElementById('nav-btn3');
+
+// Add a click event listener to the button
+button3.addEventListener('click', function() {
+  // Change the window location to a new URL
+  window.location.href ="bags3.html";  // Replace with your desired URL
+});
+
+const button4 = document.getElementById('nav-btn4');
+button4.addEventListener('click', function() {
+  // Change the window location to a new URL
+  window.location.href ="bags4.html";  // Replace with your desired URL
+});
+
---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+// You can add interactive JavaScript here if needed. For now, the Buy Now button shows an alert.
+function addToCart() {
+    alert("Item added to cart!");
+  }
+  
